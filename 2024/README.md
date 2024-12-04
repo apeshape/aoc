74a86827c79029355ecfccd13d6b6d0231646688
Anders <anders.andersson@apeshape.se>
2024-12-04 08:34:48 +0100

    day 4

--- 2024/README.md
+++ 2024/README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: 2.758ms
 
 ```
 Day 03
-Time part 1: 0.158ms
-Time part 2: 0.37ms
-Both parts: 0.528ms
+Time part 1: 0.2ms
+Time part 2: 0.363ms
+Both parts: 0.563ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 4.927ms
+Time part 2: 2.573ms
+Both parts: 7.5ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 10.005ms
+Total stars: 8/50
+Total time: 17.54ms
 ```
 
 <!--/RESULTS-->
---
+++ 2024/src/day04/index.ts
@@ -0,0 +1,135 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((l) => l.split(""));
+const dirs = [
+  //[y, x]
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+];
+
+const part1 = (rawInput: string) => {
+  const grid = parseInput(rawInput);
+
+  const testPos = (x: number, y: number) => {
+    const found = [];
+    dirs.forEach((dir) => {
+      let str = "";
+      let curPos = [x, y];
+      while (str.length < 4) {
+        if (grid[curPos[1]]?.[curPos[0]]) {
+          str += grid[curPos[1]][curPos[0]];
+          curPos = [curPos[0] + dir[1], curPos[1] + dir[0]];
+        } else {
+          break;
+        }
+      }
+      if (str === "XMAS" || str === "SAMX") {
+        found.push(str);
+      }
+    });
+    return found.length;
+  };
+
+  let score = 0;
+  for (let y = 0; y < grid.length; y++) {
+    const row = grid[y];
+    for (let x = 0; x < row.length; x++) {
+      const char = grid[y][x];
+      if (char === "X") {
+        score += testPos(x, y);
+      }
+    }
+  }
+
+  return score;
+};
+
+const part2 = (rawInput: string) => {
+  const grid = parseInput(rawInput);
+
+  const testPos = (x: number, y: number) => {
+    const maxX = x + 2;
+    const maxY = y + 2;
+    if (grid[maxY]?.[maxX] === undefined) {
+      return 0;
+    }
+
+    const topLeft = grid[y][x];
+    const topRight = grid[y][x + 2];
+    const bottomRight = grid[y + 2][x + 2];
+    const bottomLeft = grid[y + 2][x];
+    const middle = grid[y + 1][x + 1];
+    const first = [topLeft, middle, bottomRight].join("");
+    const second = [topRight, middle, bottomLeft].join("");
+
+    if (
+      (first === "MAS" || first === "SAM") &&
+      (second === "MAS" || second === "SAM")
+    ) {
+      return 1;
+    }
+
+    return 0;
+  };
+
+  let score = 0;
+  for (let y = 0; y < grid.length; y++) {
+    const row = grid[y];
+    for (let x = 0; x < row.length; x++) {
+      const char = grid[y][x];
+      if (["M", "S"].includes(char)) {
+        score += testPos(x, y);
+      }
+    }
+  }
+
+  return score;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`,
+        expected: 18,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`,
+        expected: 9,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
